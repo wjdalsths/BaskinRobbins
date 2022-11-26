36729d651f3318c6c364eed97e5be46aa8f17d8a
Jeongmin <srm0609@naver.com>
2022-11-26 15:39:08 +0900

    footer

---
+++ baskinrobbins/components/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+import * as S from "./style";
+
+const Footer = () => {
+  return (
+    <>
+      <S.Container>
+        <S.Info>
+          <p>사업자 등록번호 : 333-22-55555</p>
+          <p>(주)베라를지켜츄</p>
+          <p>광주광역시 광산구 송정동 상무대로 312</p>
+        </S.Info>
+        <S.Info>TEL : 000-3333-1111 개인정보관리책임자 : 000</S.Info>
+        <p>Copyright ⓒ 2022 BRCKOREA Company. All Rights Reserved.</p>
+      </S.Container>
+    </>
+  );
+};
+
+export default Footer;
---
+++ baskinrobbins/components/Footer/style.ts
@@ -0,0 +1,23 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  width: 100%;
+  margin-top: 80px;
+  bottom: 0;
+  font-size: 12px;
+  text-align: center;
+  p {
+    color: #b1adab;
+  }
+  font-family: "베스킨라빈스B";
+`;
+
+export const Info = styled.div`
+  display: flex;
+  justify-content: center;
+  p {
+    color: black;
+    margin: 0;
+    padding-right: 15px;
+  }
+`;
